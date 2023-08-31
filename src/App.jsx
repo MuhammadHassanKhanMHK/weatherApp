58393fdc80d7e76701c9bf2eee4d9eaaf6612fa5
MuhammadHassanKhanMHK <khanhassan979@gmail.com>
2023-08-31 09:52:45 +0500

    Weather

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App(props) {
+  const { wheatherdata } = props;
+  const [data, setData] = useState([]);
+  // console.log(wheatherdata)
+  useEffect(() => {
+    if (wheatherdata) {
+      setData((prevData) => [...prevData, wheatherdata]);
+    }
+
+  }, [wheatherdata]);
+  // useEffect(() => {
+  //   data.reverse();
+  // }, [data]);
+
+  return (
+    <>
+      <div className="container-fluid px-1 px-md-4 mx-auto">
+
+        {data.map((item, index) => (
+          <div className="row d-flex justify-content-center px-3" key={index}>
+            <div className="card animate-card item-center ">
+
+              <h2 className="ml-auto mr-4 mt-3 mb-0">
+                {item?.data?.location?.name}
+              </h2>
+              <p className="ml-auto mr-4 mb-0 med-font">
+                {item?.data?.current?.condition?.text}
+              </p>
+              <h1 className="ml-auto mr-4 large-font">
+                {item?.data?.current?.temp_c}
+                &#176;
+              </h1>
+              <p className="time-font mb-0 ml-4 mt-auto">
+                {item?.data?.location?.country}
+
+                {/* <span className="sm-font">AM</span> */}
+              </p>
+              <div className="weather-image d-none d-sm-block">
+                <img src={`https:${item?.data?.current?.condition?.icon}`} alt="Weather" />
+              </div>
+              <p className="ml-4 mb-4">
+                {item?.data?.location?.localtime}
+              </p>
+
+            </div>
+          </div>
+
+        )).reverse()}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Input.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useRef } from "react";
+import axios from "axios";
+import "./input.css";
+import App from "../App";
+function Input() {
+  let iValue = useRef(null);
+  const [apiData , setApiData] = useState(null);
+  const [err , setErr] = useState("")
+  const callApi = async (e) => {
+    e.preventDefault();
+    // console.log(getinput);
+    // console.log(iValue.current.value);
+    let cityName = iValue?.current?.value;
+    const BASE_URL = "https://api.weatherapi.com/v1/forecast.json?key=";
+    const APIKEY = "09e23a81c17e423093b64454230607";
+    try {
+      if(cityName === "" ){
+          setErr("City Name Required!")
+      }else{
+        const res = await axios.get(`${BASE_URL}${APIKEY}&q=${cityName}&aqi=no`);
+        setApiData(res)
+        iValue.current.value = ""
+        setErr("")
+      }
+   
+      
+      // console.log(res);
+    } catch (e) {
+      console.log(e.response.data.error.message);
+      setErr(e.response.data.error.message)
+    }
+  };
+  return (
+    <>
+      <div className="container-fluid sticky-top">
+        <div className="search-bar" id="searchBar">
+          <input
+            className="form-control rounded"
+            placeholder="Enter City Name"
+            type="text"
+            id="searchBar"
+            maxLength={20}
+            minLength={2}
+            required
+            ref={iValue}
+          />
+          <button type="button" className="btn btn-dark" onClick={callApi}>
+            Check!
+          </button>
+        </div>
+      </div>
+      <p className="text-danger text-center fw-bold">{err}</p>
+      <App wheatherdata ={apiData}></App>
+    </>
+  );
+}
+
+export default Input;
